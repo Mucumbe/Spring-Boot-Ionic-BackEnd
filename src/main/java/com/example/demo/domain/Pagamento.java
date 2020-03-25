3b3e95b0d9d2ae128ca27ce8bd2976b48e3191d1
Mucumbe <Mucumbeblandino@gmail.com>
2020-03-25 21:59:58 +0200

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/example/demo/domain/Pagamento.java
+++ src/main/java/com/example/demo/domain/Pagamento.java
@@ -11,29 +11,29 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.example.demo.domain.enums.EstadoPagamento;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class Pagamento implements Serializable{
-	
+public abstract class Pagamento implements Serializable {
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private Integer id;
 	private Integer estado;
-	
-	@JsonBackReference
+
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
 	private Pedido pedido;
-	
+
 	public Pagamento() {
-		
+
 	}
 
 	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
@@ -91,7 +91,5 @@ public abstract class Pagamento implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
 
 }
