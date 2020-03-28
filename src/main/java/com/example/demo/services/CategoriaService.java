3a34e1d8940aa86fac800b85631b34a4d85ee304
Mucumbe <Mucumbeblandino@gmail.com>
2020-03-28 04:00:44 +0200

    paginacao com parametros opcionais na requisicao

--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.Repositories.CategoriaRepository;
@@ -18,40 +21,44 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	
-
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(()->new ObjectNotFoundException(
-				"Objecto não encontrado! ID: "+id+", Tipo"+Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objecto não encontrado! ID: " + id + ", Tipo" + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
 	public void delete(Integer id) {
 		find(id);
-		
+
 		try {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Nao é Possivel Excluir uma Categoria Que Possui Produtos");
-			
+
 		}
-		
-		
+
 	}
-	
-	public List <Categoria> findAll(){
-		
+
+	public List<Categoria> findAll() {
+
 		return repo.findAll();
 	}
 	
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage,String orderBy,String direction){
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		return repo.findAll(pageRequest);
+	}
+
 }
