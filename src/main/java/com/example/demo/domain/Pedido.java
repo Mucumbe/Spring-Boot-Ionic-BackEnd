e159b11d35e904fc8329a67acc27cf96debe34fe
Mucumbe <Mucumbeblandino@gmail.com>
2020-04-14 06:55:04 +0200

    implementando toString de Pedido

--- src/main/java/com/example/demo/domain/Pedido.java
+++ src/main/java/com/example/demo/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.example.demo.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -28,15 +31,13 @@ public class Pedido implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@JsonFormat(pattern = "dd/MM/yyy HH:mm")
 	private Date instante;
 
-	
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 
-	
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
@@ -70,12 +71,13 @@ public class Pedido implements Serializable {
 	}
 
 	public Double getValorTotal() {
-		Double soma=0.0;
-		for(ItemPedido ip: itens)
-			soma=soma+ip.getSubTotal();
+		Double soma = 0.0;
+		for (ItemPedido ip : itens)
+			soma = soma + ip.getSubTotal();
 		return soma;
-		
+
 	}
+
 	public Integer getId() {
 		return id;
 	}
@@ -108,6 +110,14 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -133,12 +143,25 @@ public class Pedido implements Serializable {
 		return true;
 	}
 
-	public Set<ItemPedido> getItens() {
-		return itens;
-	}
-
-	public void setItens(Set<ItemPedido> itens) {
-		this.itens = itens;
+	@Override
+	public String toString() {
+		NumberFormat nf= NumberFormat.getCurrencyInstance(new Locale("pt","MZ"));
+		SimpleDateFormat sdf= new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido Numero: ");
+		builder.append(getId());
+		builder.append(", Instante: ");
+		builder.append(sdf.format(getInstante()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situação do Pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\nDetalhes\n");
+		for(ItemPedido ip:getItens())
+			builder.append(ip.toString());
+		builder.append("Valor Total: ");
+		builder.append(nf.format(getValorTotal()));
+		return builder.toString();
 	}
 
 }
--- src/main/java/com/example/demo/services/PedidoService.java
+++ src/main/java/com/example/demo/services/PedidoService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.Repositories.ItemPedidoRepository;
 import com.example.demo.Repositories.PagamentoRepository;
@@ -32,6 +33,9 @@ public class PedidoService {
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -39,9 +43,11 @@ public class PedidoService {
 				"Objecto não encontrado! ID: "+id+", Tipo"+Pedido.class.getName()));
 	}
 	
+	@Transactional	
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		
@@ -55,10 +61,13 @@ public class PedidoService {
 		
 		for(ItemPedido ip: obj.getItens()) {
 			ip.setDesconto(0.0);
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
 			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		
+		System.out.println(obj);
 		
 		return obj;
 		
