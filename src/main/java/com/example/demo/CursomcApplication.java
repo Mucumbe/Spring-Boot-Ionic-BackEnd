c3c7ae2e46a541b5b42058df2aa185026d27a55c
Mucumbe <Mucumbeblandino@gmail.com>
2020-04-15 09:43:01 +0200

    MockEmailService com Logger. Padrões Strategy e Template

--- src/main/java/com/example/demo/CursomcApplication.java
+++ src/main/java/com/example/demo/CursomcApplication.java
@@ -1,35 +1,11 @@
 package com.example.demo;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.demo.Repositories.CategoriaRepository;
-import com.example.demo.Repositories.CidadeRepository;
-import com.example.demo.Repositories.ClienteRepository;
-import com.example.demo.Repositories.EnderecoRepository;
-import com.example.demo.Repositories.EstadoRepository;
-import com.example.demo.Repositories.ItemPedidoRepository;
-import com.example.demo.Repositories.PagamentoRepository;
-import com.example.demo.Repositories.PedidoRepository;
-import com.example.demo.Repositories.ProdutoRepository;
-import com.example.demo.domain.Categoria;
-import com.example.demo.domain.Cidade;
-import com.example.demo.domain.Cliente;
-import com.example.demo.domain.Endereco;
-import com.example.demo.domain.Estado;
-import com.example.demo.domain.ItemPedido;
-import com.example.demo.domain.Pagamento;
-import com.example.demo.domain.PagamentoComBoleto;
-import com.example.demo.domain.PagamentoComCartao;
-import com.example.demo.domain.Pedido;
-import com.example.demo.domain.Produto;
-import com.example.demo.domain.enums.EstadoPagamento;
-import com.example.demo.domain.enums.TipoCliente;
+
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
