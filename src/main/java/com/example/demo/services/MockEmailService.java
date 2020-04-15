b211eff94c4470ef3867d286e3ae06582399bff6
Mucumbe <Mucumbeblandino@gmail.com>
2020-04-15 23:37:39 +0200

    Email Html

--- src/main/java/com/example/demo/services/MockEmailService.java
+++ src/main/java/com/example/demo/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.example.demo.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -12,6 +14,14 @@ public class MockEmailService extends AbstractEmailService {
     public void sendEmail(SimpleMailMessage msg) {
 	
 	LOG.info("Simulando Envio de Email...");
+	LOG.info(msg.toString());
+	LOG.info("Email Enviado");
+	
+    }
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+	LOG.info("Simulando Envio de Email Html...");
 	LOG.info(msg.toString());
 	LOG.info("Email Enviado");
 	
--- src/main/java/com/example/demo/services/SmtpEmailService.java
+++ src/main/java/com/example/demo/services/SmtpEmailService.java
@@ -1,17 +1,23 @@
 package com.example.demo.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
-public class SmtpEmailService extends AbstractEmailService{
-    
+public class SmtpEmailService extends AbstractEmailService {
+
     @Autowired
     private MailSender mailSender;
     
-    private static final Logger LOG= LoggerFactory.getLogger(SmtpEmailService.class);
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 
     @Override
     public void sendEmail(SimpleMailMessage msg) {
@@ -19,8 +25,13 @@ public class SmtpEmailService extends AbstractEmailService{
 	mailSender.send(msg);
 	LOG.info("Email Enviado");
     }
-    
-  
-    
+
+    @Override
+    public void sendHtmlEmail(MimeMessage msg) {
+	LOG.info("Simulando Envio de Email...");
+	javaMailSender.send(msg);
+	LOG.info("Email Enviado");
+
+    }
 
 }
