9743990bd39c09d7bee71205fd47899bd731bdbd
Mucumbe <Mucumbeblandino@gmail.com>
2020-04-27 12:58:23 +0200

    Endpoint para Buscar Cliente por Email

--- src/main/java/com/example/demo/services/ClienteService.java
+++ src/main/java/com/example/demo/services/ClienteService.java
@@ -43,17 +43,15 @@ public class ClienteService {
 
     @Autowired
     private S3Service s3Service;
-    
+
     @Autowired
     private ImageService imageService;
-    
+
     @Value("${img.prefix.client.profile}")
     private String prefix;
-    
+
     @Value("${img.profile.size}")
     private Integer size;
-    
-   
 
     public Cliente find(Integer id) {
 
@@ -96,6 +94,20 @@ public class ClienteService {
     public List<Cliente> findAll() {
 
 	return repo.findAll();
+    }
+
+    public Cliente findByEmail(String email) {
+	UserSS user = UserService.authenticated();
+	if (user == null || !user.hasRole(Perfil.ADMIN) && !email.equals(user.getUsername())) {
+	    throw new AuthorizationException("Acesso negado");
+	}
+
+	Cliente obj = repo.findByEmail(email);
+	if (obj == null) {
+	    throw new ObjectNotFoundException(
+		    "Objeto não encontrado! Id: " + user.getId() + ", Tipo: " + Cliente.class.getName());
+	}
+	return obj;
     }
 
     public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
@@ -142,13 +154,12 @@ public class ClienteService {
 	UserSS user = UserService.authenticated();
 	if (user == null)
 	    throw new AuthorizationException("Acesso Negado");
-	
+
 	BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
-	jpgImage= imageService.cropSquare(jpgImage);
-	jpgImage= imageService.resize(jpgImage, size);
-	
-	String fileName =prefix+user.getId()+".jpg";
-	
+	jpgImage = imageService.cropSquare(jpgImage);
+	jpgImage = imageService.resize(jpgImage, size);
+
+	String fileName = prefix + user.getId() + ".jpg";
 
 	return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
     }
